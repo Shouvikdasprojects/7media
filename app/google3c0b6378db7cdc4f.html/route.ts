d2b2d7595cc0ae701b29f8a3dfc6ff323e739b9b
Shouvik Das <shouvikdas155@gmail.com>
2026-08-30 14:08:49 +0530

    feat(seo): add route handler for google3c0b6378db7cdc4f.html

---
+++ app/google3c0b6378db7cdc4f.html/route.ts
@@ -0,0 +1,7 @@
+﻿export async function GET() {
+  return new Response('google-site-verification: google3c0b6378db7cdc4f.html', {
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+  })
+}
